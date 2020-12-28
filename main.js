d235f6653d2f63d200d9829d332cf03808468499
jjm2317 <wlaks2317@gmail.com>
2020-12-28 10:27:57 +0900

    feat: Make generating func

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 //crypto 모듈 : 암호화 목적
 const { POINT_CONVERSION_COMPRESSED } = require('constants');
 const crypto = require('crypto');
+const { create } = require('domain');
 //파일 시스템 
 const fs = require('fs');
 
 
-const privateKeyFileName = 'C:/Users/atlas/Documents/signPem/pri_signtest.myskcdn.net.pem';
+const privateKeyFileName =;
 fs.readFile(privateKeyFileName, (err, data) => console.log(data));
-const keyPairId = '';
-const urlPath = '';
+const keyPairId = 'pub_signtest.myskcdn.net';
+const urlPath = 'http://signtest.myskcdn.net/skcdn.mp4';
 const expires = Date.now() + 21600;
-const clientIp = '';
+const clientIp = '0.0.0.0/0';
 const waterMark = "1010101101"
 //signed url policy : 3가지 => 경로확인, url 만료 기간, 요청 ip 주소 확인 
 const policy = JSON.stringify({
@@ -53,20 +54,28 @@ const urlSafeBase64Encode = (value) => {
 console.log(urlSafeBase64Encode(rsaSha1Sign(policy, privateKeyFileName)));
 
 
-const createStreamName = (urlPath, policy, signature, keyPairId, expires) => {
+const createStreamName = (stream, policy, signature, keyPairId, expires) => {
     let path = "";
-    let result = urlPath;
-    const separator = urlPath.match(/?/) ? '&' : '?';
+    let result = stream;
+    const separator = stream.match(/\?/) ? '&' : '?';
     if (expires) {
         result += `${path + separator}Expires=${expires}&Signature=${signature}&Key-Pair-Id=${keyPairId}`;
     } else {
-        result += `${path + separator}Policy=${Policy}&Signature=${signature}&Key-Pair-Id=${keyPairId}`;
+        result += `${path + separator}Policy=${policy}&Signature=${signature}&Key-Pair-Id=${keyPairId}`;
     }
     return result.replace(/\n/ig, '');
 };
 
 const encodeQueryParams = () => { };
 
-const getCustomPolicyStreamName = () => { };
+const getCustomPolicyStreamName = (urlPath, privateKeyFileName, keyPairId, policy) => {
+    const encodedPolicy = urlSafeBase64Encode(policy);
+    const signature = rsaSha1Sign(policy, privateKeyFileName);
+    const encodedSignature = urlSafeBase64Encode(signature);
 
-const customPolicyStreamName = getCustomPolicyStreamName()
+    const streamName = createStreamName(urlPath, encodedPolicy, encodedSignature, keyPairId, expires);
+    return streamName;
+};
+
+const customPolicyStreamName = getCustomPolicyStreamName(urlPath, privateKeyFileName, keyPairId, policy);
+console.log(customPolicyStreamName);
